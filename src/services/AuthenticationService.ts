30b8dc1db72a2aa2f650e13cfae02b115c046066
Felipe <jflobom@gmail.com>
2021-12-23 01:13:45 -0500

    add logout function

--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -1,7 +1,9 @@
+import { toast } from "react-hot-toast";
 import { apiService } from "./apiService";
 import { UserLogInCredentialsDTO } from "../models/DTO/user/UserLogInCredentialsDTO";
 import UserTokenDTO from "../models/DTO/user/UserTokenDTO";
 import SessionStorageKeysEnum from "../shared/utils/enums/SessionStorageKeysEnum";
+import RoutesEnum from "../routes/RoutesEnum";
 
 export default class AuthenticationService {
   public static async login({
@@ -21,5 +23,11 @@ export default class AuthenticationService {
     );
 
     return Boolean(hasSessionToken);
+  }
+
+  public static logout() {
+    toast.success("Hasta Luego! 🤖");
+    sessionStorage.removeItem(SessionStorageKeysEnum.TOKEN);
+    window.location.assign(RoutesEnum.login);
   }
 }
